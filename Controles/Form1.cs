75a8bd522e8e901eaa10ec6de1e22186d6e19c11
nicos92 <nicolassandoval140692@gmail.com>
2025-01-08 22:22:34 -0300

    text box

---
+++ Controles/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void NsTextBox2__TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+
+        }
+
+        private void nsTextBox2__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nsTextBox2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
