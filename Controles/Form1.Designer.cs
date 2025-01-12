5a70b6acebacc4e8e4a576a67a6f6f2dfb99a700
nicos92 <nicolassandoval140692@gmail.com>
2025-01-12 19:35:18 -0300

    elimando cosas del form1

--- Controles/Form1.Designer.cs
+++ Controles/Form1.Designer.cs
@@ -28,29 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.nsDatePicker1 = new Controles.NSDatePicker();
             this.SuspendLayout();
-            // 
-            // nsDatePicker1
-            // 
-            this.nsDatePicker1.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.nsDatePicker1.BorderSize = 0;
-            this.nsDatePicker1.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right;
-            this.nsDatePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
-            this.nsDatePicker1.Location = new System.Drawing.Point(326, 153);
-            this.nsDatePicker1.MinimumSize = new System.Drawing.Size(0, 35);
-            this.nsDatePicker1.Name = "nsDatePicker1";
-            this.nsDatePicker1.Size = new System.Drawing.Size(296, 35);
-            this.nsDatePicker1.SkinColor = System.Drawing.Color.MediumSlateBlue;
-            this.nsDatePicker1.TabIndex = 0;
-            this.nsDatePicker1.TextColor = System.Drawing.Color.White;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.nsDatePicker1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -58,8 +42,6 @@
         }
 
         #endregion
-
-        private NSDatePicker nsDatePicker1;
     }
 }
 
